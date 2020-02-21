01f37db29dad76a7b81e0e010551450436c8379e
Sophia mehdizadeh <smehdizadeh7@gatech.edu>
2020-02-21 18:11:47 -0500

    Paired programming ex.3 Rishi/Sophia

--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -1,11 +1,12 @@
 
 #include <iostream>
+#include <fstream>
 #include <ctime>
+#include <Fft.h>
 
 #include "MUSI6106Config.h"
 
 #include "AudioFileIf.h"
-//#include "CombFilterIf.h"
 
 using std::cout;
 using std::endl;
@@ -20,91 +21,117 @@ int main(int argc, char* argv[])
     std::string             sInputFilePath,                 //!< file paths
                             sOutputFilePath;
 
-    static const int        kBlockSize = 1024;
+    int                     kBlockSize,
+                            kHopSize;
 
     clock_t                 time = 0;
 
-    float                   **ppfAudioData = 0;
+    float                   **ppfAudioData = 0,
+                            *pComplexSpectrum = 0,
+                            *pMagSpectrum = 0,
+                            *pInputBuffer = 0;
 
     CAudioFileIf            *phAudioFile = 0;
     std::fstream            hOutputFile;
     CAudioFileIf::FileSpec_t stFileSpec;
 
-    //CCombFilterIf   *pInstance = 0;
-    //CCombFilterIf::create(pInstance);
+    CFft                    *pCFft;
+
     showClInfo();
 
     //////////////////////////////////////////////////////////////////////////////
     // parse command line arguments
-    if (argc < 2)
-    {
-        cout << "Missing audio input path!";
+
+    sInputFilePath = argv[1];
+    sOutputFilePath = sInputFilePath + "output.txt";
+    kBlockSize = atoi(argv[2]);
+    kHopSize = atoi(argv[3]);
+
+    if(kHopSize > kBlockSize)
         return -1;
-    }
-    else
-    {
-        sInputFilePath = argv[1];
-        sOutputFilePath = sInputFilePath + ".txt";
-    }
 
     //////////////////////////////////////////////////////////////////////////////
     // open the input wave file
+
+    stFileSpec.eFormat = CAudioFileIf::FileFormat_t::kFileFormatWav;
+    stFileSpec.eBitStreamType = CAudioFileIf::BitStream_t::kFileBitStreamInt16;
+    stFileSpec.iNumChannels = 2;
+    stFileSpec.fSampleRateInHz = 44100;
+    
     CAudioFileIf::create(phAudioFile);
-    phAudioFile->openFile(sInputFilePath, CAudioFileIf::kFileRead);
-    if (!phAudioFile->isOpen())
-    {
-        cout << "Wave file open error!";
-        return -1;
-    }
-    phAudioFile->getFileSpec(stFileSpec);
+    phAudioFile->openFile(sInputFilePath, CAudioFileIf::FileIoType_t::kFileRead, &stFileSpec);
 
     //////////////////////////////////////////////////////////////////////////////
     // open the output text file
-    hOutputFile.open(sOutputFilePath.c_str(), std::ios::out);
-    if (!hOutputFile.is_open())
-    {
-        cout << "Text file open error!";
-        return -1;
-    }
+    hOutputFile.open(sOutputFilePath, std::fstream::out);
 
     //////////////////////////////////////////////////////////////////////////////
     // allocate memory
-    ppfAudioData = new float*[stFileSpec.iNumChannels];
-    for (int i = 0; i < stFileSpec.iNumChannels; i++)
-        ppfAudioData[i] = new float[kBlockSize];
+//    int sizeOfBuffer = sizeof(float) * kBlockSize;
+//    int sizeOfSpectrum = sizeof(float) * kBlockSize;
 
-    time = clock();
+    int sizeOfBuffer = kHopSize;
+    int sizeOfSpectrum = kBlockSize;
+
+//    ppfAudioData = (float**) malloc(sizeof(float) * 2);
+    ppfAudioData = new float*[2];
+    *ppfAudioData = new float [kHopSize];
+    *(ppfAudioData+1) = new float [kHopSize];
+
+    pMagSpectrum = new float [sizeOfBuffer+1];
+    pComplexSpectrum = new float [sizeOfSpectrum];
+
+    pInputBuffer = new float [kBlockSize]();
+
     //////////////////////////////////////////////////////////////////////////////
-    // get audio data and write it to the output file
-    while (!phAudioFile->isEof())
-    {
-        long long iNumFrames = kBlockSize;
-        phAudioFile->readData(ppfAudioData, iNumFrames);
+    // Create FFT instance and init
 
-        cout << "\r" << "reading and writing";
+    CFft::createInstance(pCFft);
+    pCFft->initInstance(kBlockSize);
 
-        for (int i = 0; i < iNumFrames; i++)
-        {
-            for (int c = 0; c < stFileSpec.iNumChannels; c++)
-            {
-                hOutputFile << ppfAudioData[c][i] << "\t";
-            }
-            hOutputFile << endl;
+    //////////////////////////////////////////////////////////////////////////////
+    // get audio data and write it to the output text file (one column per channel)
+    long long fileData = sizeOfBuffer;
+
+    while(fileData!=0) {
+        phAudioFile->readData(ppfAudioData, fileData);
+
+        for (int i = 0; i < fileData; i++) {
+            pInputBuffer[kHopSize + i] = ppfAudioData[0][i];
         }
-    }
 
-    cout << "\nreading/writing done in: \t" << (clock() - time)*1.F / CLOCKS_PER_SEC << " seconds." << endl;
+        pCFft->doFft(pComplexSpectrum, pInputBuffer);
 
+        pCFft->getMagnitude(pMagSpectrum, pComplexSpectrum);
+
+        for (int i = 0; i < sizeOfBuffer; i++) {
+            pInputBuffer[i] = pInputBuffer[kHopSize + i];
+        }
+
+        for(int buf = 0; buf < sizeOfBuffer+1; buf++) {
+            hOutputFile << pMagSpectrum[buf] << endl;
+            }
+        }
+    
     //////////////////////////////////////////////////////////////////////////////
-    // clean-up
+    // clean-up (close files and free memory)
+    delete *ppfAudioData;
+    delete *(ppfAudioData+1);
+    delete ppfAudioData;
+
+    delete[] pInputBuffer;
+    delete[] pComplexSpectrum;
+    delete[] pMagSpectrum;
+
+    pInputBuffer = 0;
+    pComplexSpectrum = 0;
+    pMagSpectrum = 0;
+    
     CAudioFileIf::destroy(phAudioFile);
+    CFft::destroyInstance(pCFft);
     hOutputFile.close();
 
-    for (int i = 0; i < stFileSpec.iNumChannels; i++)
-        delete[] ppfAudioData[i];
-    delete[] ppfAudioData;
-    ppfAudioData = 0;
-
+    // all done
     return 0;
 
 }
@@ -118,4 +145,3 @@ void     showClInfo()
 
     return;
 }
-
